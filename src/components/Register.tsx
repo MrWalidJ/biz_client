5d0e6bf778115a536dfe98553961d7626a7683de
MrWalidJ <wjamjoum1@gmail.com>
2022-11-22 08:39:19 +0200

    comment  console log in Register

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -8,12 +8,12 @@ import { addUser } from "../services/usersService";
 import Navbar from "./Navbar";
 
 interface RegisterProps {
-  Biz: boolean,
-  text:string,
-  image:string,
+  Biz: boolean;
+  text: string;
+  image: string;
 }
 
-const Register: FunctionComponent<RegisterProps> = ({ Biz, text,image }) => {
+const Register: FunctionComponent<RegisterProps> = ({ Biz, text, image }) => {
   const navigate = useNavigate();
   const formik = useFormik({
     initialValues: { name: "", email: "", password: "" },
@@ -25,9 +25,9 @@ const Register: FunctionComponent<RegisterProps> = ({ Biz, text,image }) => {
     onSubmit: (values) => {
       let user: User = { ...values, biz: Biz };
       addUser(user)
-        // .then((result) => console.log(result.data))
+        
         .then((result) => {
-          console.log(result.data);
+          //console.log(result.data);
           sessionStorage.setItem("token", result.data.token); // save the token in session storage
           successMsg("successfully registered !"); // still needs html
           navigate("/signin");
@@ -42,74 +42,74 @@ const Register: FunctionComponent<RegisterProps> = ({ Biz, text,image }) => {
   return (
     <>
       <Navbar />
- <div className="row m-3">
-      <form className="mx-2 w-25 col-md-4" onSubmit={formik.handleSubmit}>
-        <h3 className="display-5 text-center">SIGN UP</h3>
-        <h5> {text} </h5>
-        <div className="mb-3 form-floating">
-          <input
-            type="text"
-            className="form-control"
-            id="inputName"
-            name="name"
-            value={formik.values.name}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            placeholder="Name"
-          />
-          <label htmlFor="inputName">Name</label>
+      <div className="row m-3">
+        <form className="mx-2 w-25 col-md-4" onSubmit={formik.handleSubmit}>
+          <h3 className="display-5 text-center">SIGN UP</h3>
+          <h5> {text} </h5>
+          <div className="mb-3 form-floating">
+            <input
+              type="text"
+              className="form-control"
+              id="inputName"
+              name="name"  
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              placeholder="Name"
+            />
+            <label htmlFor="inputName">Name</label>
+          </div>
+          {formik.touched.name && formik.errors.name ? (
+            <p className="text-danger">{formik.errors.name}</p>
+          ) : null}
+          <div className="mb-3 form-floating">
+            <input
+              type="email"
+              className="form-control"
+              id="inputEmail"
+              name="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              placeholder="Email"
+            />
+            <label htmlFor="inputEmail">Email address</label>
+          </div>
+          {formik.touched.email && formik.errors.email ? (
+            <p className="text-danger">{formik.errors.email}</p>
+          ) : null}
+          <div className="mb-3 form-floating">
+            <input
+              type="password"
+              className="form-control"
+              id="inputPassword"
+              name="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              placeholder="Password"
+            />
+            <label htmlFor="inputPassword">Password</label>
+          </div>
+          {formik.touched.password && formik.errors.password ? (
+            <p className="text-danger">{formik.errors.password}</p>
+          ) : null}
+          <div className="text-center">
+            <button
+              type="submit"
+              className="btn btn-success w-50"
+              disabled={!(formik.isValid && formik.dirty)}
+            >
+              Submit
+            </button>
+            <p className="text-center mt-3">
+              <Link to="/">Already a user? click here </Link>
+            </p>
+          </div>
+        </form>
+        <div className="col-md-8">
+          <img src={image} alt="" className="w-100" />
         </div>
-        {formik.touched.name && formik.errors.name ? (
-          <p className="text-danger">{formik.errors.name}</p>
-        ) : null}
-        <div className="mb-3 form-floating">
-          <input
-            type="email"
-            className="form-control"
-            id="inputEmail"
-            name="email"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            placeholder="Email"
-          />
-          <label htmlFor="inputEmail">Email address</label>
-        </div>
-        {formik.touched.email && formik.errors.email ? (
-          <p className="text-danger">{formik.errors.email}</p>
-        ) : null}
-        <div className="mb-3 form-floating">
-          <input
-            type="password"
-            className="form-control"
-            id="inputPassword"
-            name="password"
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            placeholder="Password"
-          />
-          <label htmlFor="inputPassword">Password</label>
-        </div>
-        {formik.touched.password && formik.errors.password ? (
-          <p className="text-danger">{formik.errors.password}</p>
-        ) : null}
-        <div className="text-center">
-          <button
-            type="submit"
-            className="btn btn-success w-50"
-            disabled={!(formik.isValid && formik.dirty)}
-          >
-            Submit
-          </button>
-          <p className="text-center mt-3">
-            <Link to="/">Already a user? click here </Link>
-          </p>
-        </div>
-      </form>
-      <div className="col-md-8">
-        <img src={image} alt="" className="w-100"  />
-      </div>
       </div>
     </>
   );
